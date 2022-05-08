3dbf19af2f4680c36c37a131f5396335c977a5e1
dazhuang6 <wangxinbao_wxb@163.com>
2022-05-08 19:51:31 +0800

    UA and DAU commit

--- src/main/java/com/qshp/community/config/WebMvcConfig.java
+++ src/main/java/com/qshp/community/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.qshp.community.config;
 
+import com.qshp.community.controller.interceptor.DataInterceptor;
 import com.qshp.community.controller.interceptor.LoginRequiredInterceptor;
 import com.qshp.community.controller.interceptor.LoginTicketInterceptor;
 import com.qshp.community.controller.interceptor.MessageInterceptor;
@@ -21,6 +22,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Resource
     private MessageInterceptor messageInterceptor;
 
+    @Resource
+    private DataInterceptor dataInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 
@@ -31,6 +35,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 //                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
 
         registry.addInterceptor(messageInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+
+        registry.addInterceptor(dataInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
 
     }
